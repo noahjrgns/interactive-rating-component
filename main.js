81213d6bd772a20f22a01cf1d31710991a9b4a04
noahjrgns <112009547+noahjrgns@users.noreply.github.com>
2022-09-09 21:39:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,13 @@
 let selection  = 0
 
-function selection1() {
-    selection = 1
-}
-
-function selection2() {
-    selection = 2
-}
-
-function selection3() {
-    selection = 3
-}
-
-function selection4() {
-    selection = 4
-}
-
-function selection5() {
-    selection = 5
+function select(i) {
+    selection = i
 }
 
 
 
 function submit() {
     let main = document.querySelector("main")
-    let btn = document.querySelector(".btn")
 
     if (selection < 1) {
         btn.textContent = "select your rating"
